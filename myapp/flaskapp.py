e4e265ca400eba30f9a3567bc782a7a37af63dd6
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2024-10-09 16:48:42 +0530

    Update flaskapp.py

--- myapp/flaskapp.py
+++ myapp/flaskapp.py
@@ -110,4 +110,4 @@ def download_file(filename):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(port=5001, debug=True)
+    app.run(debug=False,host='0.0.0.0')
